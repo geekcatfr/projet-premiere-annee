f6c75b8a683729525d9b20fe6ad4353b50e80b62
Harry Hauer <geekcat@protonmail.com>
2022-01-25 17:02:11 +0100

    we restart everything, let's use django

--- projet_fc_pro/site_fc_pro/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SiteFcProConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_fc_pro'
--- projet_fc_pro/site_fc_pro/urls.py
+++
@@ -1,8 +0,0 @@
-import imp
-from django import urls
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index')
-]
