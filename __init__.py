80b3f6ee921693848e249a55dc5fb8345433cfa6
Harry Hauer <geekcat@protonmail.com>
2022-01-25 17:15:56 +0100

    git init (again)

--- __init__.py
+++ __init__.py
@@ -23,9 +23,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    from . import index
+    app.register_blueprint(index.bp)
 
     return app
