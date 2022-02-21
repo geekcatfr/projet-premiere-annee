48df93b762e246347b3fac483d104cc3ecf55129
Harry Hauer <geekcat@protonmail.com>
2022-02-21 15:43:36 +0100

    Deleted Flask files

--- formations.py
+++
@@ -1,9 +0,0 @@
-import functools
-
-from flask import Blueprint, render_template
-
-bp = Blueprint('formations', __name__, url_prefix='/formations')
-
-@bp.route('/formations')
-def formations():
-    return render_template('formations.html')
