ff5f984a59bd4316336fa41dc6074bddfba7bc8a
théo <houguet.theo@gmail.com>
2022-03-01 17:56:05 +0100

    Ajout element bouton

---
+++ client/src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+import './Button.css'
+
+function Button() {
+    return ( 
+    <div >
+        <a className='button'> 
+        Formations
+        </a>
+    </div>
+    )
+}
+
+export default Button
---
+++ client/src/pages/Admin/WelcomeAdmin/WelcomeAdmin.jsx
@@ -0,0 +1,11 @@
+import './WelcomeAdmin.css'
+
+function WelcomeAdmin() {
+    return (
+        <div>
+            <div className='wrapper'>
+            <a>Quatre</a> 
+            </div>
+        </div>>
+    )
+}
