2d9cbafc9872377137cc2463dc9d672fb297f328
Harry Hauer <geekcat@protonmail.com>
2022-03-02 17:01:04 +0100

    Added API providing content to formations page

---
+++ server/main.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello", "world"}
+
+
+@app.get("/formations")
+def list_formations():
+    return {"nom": ['Economie', 'Maths'], "desc": "ceci est une formation d\'eco"}
+
+
+@app.get("/formations/{formation_id}")
+def formation_content(formation_id: int):
+    return {"id": formation_id}
