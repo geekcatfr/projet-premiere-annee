7f568ef4cce5da027a943ccf564fa1866737b02d
Harry Hauer <geekcat@protonmail.com>
2022-03-02 17:03:10 +0100

    Moved button into the page file

--- client/src/pages/Admin/WelcomeAdmin/WelcomeAdmin.jsx
+++ client/src/pages/Admin/WelcomeAdmin/WelcomeAdmin.jsx
@@ -1,11 +1,22 @@
+import React from 'react'
 import './WelcomeAdmin.css'
 
 function WelcomeAdmin() {
     return (
         <div>
-            <div className='wrapper'>
-            <a>Quatre</a> 
-            </div>
-        </div>>
+            <Button buttonText='Formations' />
+        </div>
     )
+}
+
+class Button extends React.Component {
+    constructor() {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='button'>{this.buttonText}</div>
+        )
+    }
 }
