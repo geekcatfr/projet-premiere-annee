a9bfd2ca9e5adb01dd1c860e98369e3383216933
Harry Hauer <geekcat@protonmail.com>
2022-03-04 09:19:10 +0100

    Added some buttons on the app page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
-    <div className='container'>
-      <p className='first-page-title'>Centre de formation FC PRO NDLP</p>
+    <div className="container">
+      <p className="first-page-title">Centre de formation FC PRO NDLP</p>
+      <Button name="En savoir plus" />
+      <Button name="Nos formations" />
     </div>
-  )
+  );
 }
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-} 
+function Button(props) {
+  return (
+    <>
+      <div className="button">{props.name}</div>
+    </>
+  );
+}
