c8be74e71b29a92b4bf32a64b89421b0a44a1aa1
harry <harry@babychou.me>
2022-03-04 14:58:25 +0100

    Fixed badly named component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import Navbar from './components/Navbar'
-import Formations from './routes/Formations'
+import Navbar from './components/Navbar/Navbar'
+import Formations from './routes/Formations/Formations'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
