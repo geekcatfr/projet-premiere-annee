e10a9b328696f248c84be4c2400788ffcb50281b
kity <geekcat@protonmail.com>
2022-03-05 13:26:25 +0100

    Now using a function instead of a class

--- client/src/routes/Formations/Formations.jsx
+++ client/src/routes/Formations/Formations.jsx
@@ -1,37 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 export default function Formations() {
-    const [error] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [formations, setFormations] = useState([])
+  const [error] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [formations, setFormations] = useState([]);
 
-    useEffect(() => {
-        fetch("http://127.0.0.1:8000/formations").then(res => res.json()).then(
-            (result) => {
-                console.log(result)
-                setIsLoaded(true);
-                setFormations(result.nom);
-            }
-        )
-    }, [])
-    return (
-        <div>
-            <FormationUnit name={formations} desc="Ceci est une formation d'eco" />
-        </div>
-    )
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/formations")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setIsLoaded(true);
+        setFormations(result.nom);
+      });
+  }, []);
+  return (
+    <div>
+      <FormationUnit name={formations} desc={formations} />
+    </div>
+  );
 }
 
-class FormationUnit extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="formation-block">
-                <h2>{this.props.name}</h2>
-                <p>{this.props.desc}</p>
-            </div>
-        )
-    }
-}   
+function FormationUnit(props) {
+  return (
+    <div className="formation-block">
+      <h2>{props.name}</h2>
+      <p>{props.desc}</p>
+    </div>
+  );
+}
