2e99d4db168b3c0a034939cb7a84129c56be4a90
kity <geekcat@protonmail.com>
2022-03-05 13:26:41 +0100

    Created more correct page

--- client/src/routes/Login/Login.jsx
+++ client/src/routes/Login/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import Logo from "./../../assets/icons/logo-ndlp.png";
+import "./Login.css";
+
 export default function Login() {
-    return (
-        <>Login</>
-    )
+  return (
+    <div className="container login-container">
+      <div className="login">
+        <img src={Logo} alt="Logo NDLP" />
+        <h1>Connexion</h1>
+        <form>
+          <div className="form-content">
+            <label htmlFor="email">Email :</label>
+            <input type="email" name="email" id="email" required />
+            <label htmlFor="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" />
+            <input type="submit" value="Connexion" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
