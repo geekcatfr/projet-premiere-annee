afa6ac9ee1eb4fd9d54725baa31ea2e7f1970278
kity <geekcat@protonmail.com>
2022-03-05 13:25:46 +0100

    Added new routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import Navbar from './components/Navbar/Navbar'
-import Formations from './routes/Formations/Formations'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import App from "./App";
+import Navbar from "./components/Navbar/Navbar";
+import Formations from "./routes/Formations/Formations";
+import Login from "./routes/Login/Login";
+import About from "./routes/About/About";
+
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Navbar />}>
+        <Route path="/" element={<Navbar />}>
           <Route index element={<App />} />
-          <Route path='/formations' element={<Formations />} />
+          <Route path="/formations" element={<Formations />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
