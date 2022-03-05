94eb26db8929763d7dfb403ac84d383a005417fb
kity <geekcat@protonmail.com>
2022-03-05 13:24:34 +0100

    Fixed file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,14 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="container">
-      <p className="first-page-title">Centre de formation FC PRO NDLP</p>
-      <Button name="En savoir plus" />
-      <Button name="Nos formations" />
+    <div>
+      <div className="container first-page-content">
+        <p className="first-page-title">Centre de formation FC PRO NDLP</p>
+        <div className="about-buttons">
+          <Button name="En savoir plus" />
+          <Button name="Nos formations" />
+        </div>
+      </div>
     </div>
   );
 }
