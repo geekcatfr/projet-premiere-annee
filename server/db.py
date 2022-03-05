9dab8b478084045afd6a0e82c099279d293ab99e
kity <geekcat@protonmail.com>
2022-03-05 13:54:06 +0100

    db.py connects to database now

--- server/db.py
+++ server/db.py
@@ -1,10 +1,19 @@
-from multiprocessing import connection
 import mysql.connector
 import json
 
-def loadCredentials():
-    with open('id.json') as f:
-        json.dumps(f)
-    pass
+def loadCredentials(id_file):
+    with open(id_file) as f:
+        token = json.load(f)
+        return token
 
-conn = connection.MySQLConnection()
+config = loadCredentials('db_id.json')
+
+try:
+    conn = mysql.connector.MySQLConnection(**config)
+except mysql.connector.Error as error:
+    if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif error.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(error)
