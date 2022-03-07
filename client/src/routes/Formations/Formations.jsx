48cbe0f19023bc69ebb0cb334a002aaceaff85ef
kity <geekcat@protonmail.com>
2022-03-07 22:38:30 +0100

    Added a nice error message

--- client/src/routes/Formations/Formations.jsx
+++ client/src/routes/Formations/Formations.jsx
@@ -6,32 +6,33 @@ export default function Formations() {
   const [formations, setFormations] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/formations")
+    fetch("http://localhost:8000/formations")
       .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        setIsLoaded(true);
-        setFormations(result);
-      },
-      (error) => {
+      .then(
+        (result) => {
+          console.log(result);
+          setIsLoaded(true);
+          setFormations(result);
+        },
+        (error) => {
           setIsLoaded(true);
           setError(error);
-      }
-    )
+        }
+      );
   }, []);
   if (error) {
+    console.log("Impossible to load the content of the API : " + error);
     return (
-      <>
+      <div className="container">
         <p>Erreur en essayant de charger le contenu de la page...</p>
-      </>
-    )
-  }
-  else {
-  return (
-    <div>
-      <FormationUnit name={formations.nom} desc={formations.desc} />
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <FormationUnit name={formations.nom} desc={formations.desc} />
+      </div>
+    );
   }
 }
 
