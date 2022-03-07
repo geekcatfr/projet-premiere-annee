daa5a6914c0a2ba056da8630d5db053cbb9dbc12
kity <geekcat@protonmail.com>
2022-03-07 09:11:22 +0100

    Refactored connect and init_database in db class

--- server/db.py
+++ server/db.py
@@ -20,19 +20,6 @@ def create_database(db_name, db_conn):
         print("Erreur en créant la base de données : {db_name}")
         return None
 
-def connect_database(config):
-    try:
-        conn = mysql.connector.MySQLConnection(**config)
-        return conn
-    except mysql.connector.Error as error:
-        writeInLog(error)
-        if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif error.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(error)
-
 def writeInLog(error):
     """Takes an error in str format as argument, writes in db.log the error with formatted time."""
     log_file = "db.log"
@@ -78,36 +65,57 @@ def init_rows(db_conn, db_name):
                 writeInLog(error)
 
 
-def init_database(db_conn, db_name):
-    db_cursor = db_conn.cursor()
-    try:
-        db_cursor.execute(f"USE {db_name}")
-        db_cursor = db_conn.cursor()
-    except mysql.connector.Error as error:
-        if error.errno == errorcode.ER_BAD_DB_ERROR:
+
+class DatabaseConnection():
+
+    def __init__(self):
+        self.config, self.db_creds = loadCredentials('db_id.json')
+        self.name = self.config['database']
+        
+
+    def connect(self):
+        try:
+            self.conn = mysql.connector.MySQLConnection(**self.db_creds)
+            return True
+        except mysql.connector.Error as error:
             writeInLog(error)
-            print(f"[NOTE] Database {db_name} doesn't exist. Creating it...")
-            create_database(db_name, db_cursor)
-            init_rows(db_conn, db_name)
-            db_conn.database = db_name
+            if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif error.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(error)
+            return False
+  
 
-class Database():
+    def init_database(self):
+        db_cursor = self.conn.cursor()
+        try:
+            db_cursor.execute(f"USE {self.name}")
+            db_cursor = self.conn.cursor()
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_BAD_DB_ERROR:
+                writeInLog(error)
+                print(f"[NOTE] Database {self.name} doesn't exist. Creating it...")
+                create_database(self.name, db_cursor)
+                init_rows(self.conn, self.name)
+                self.conn.database = self.name
 
-    def __init__(self) -> None:
-        pass
-    
-    def insert_data():
-
+    def insert_row(self, type, **data):
         add_user = ("INSERT INTO users "
         "(username, password)"
         "VALUES (%s %s)")
+
+    def delete_row(self) -> None:
         pass
 
-config, db_creds = loadCredentials('db_id.json')
-db_name = config['database']
+    def get_row(self, row) -> None:
+        pass
 
-db_conn = connect_database(db_creds)
+db = DatabaseConnection()
+db.connect()
 
-init_database(db_conn, db_name)
+db.init_database()
+db.insert_row('user', {'username': 'chat', 'password': 'chat'})
 
 
