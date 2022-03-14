fa95cfa633653e481ec6607bc3cead117e59392d
théo <houguet.theo@gmail.com>
2022-03-14 16:58:18 +0100

    Added link for button

--- client/src/routes/Admin/WelcomeAdmin/WelcomeAdmin.jsx
+++ client/src/routes/Admin/WelcomeAdmin/WelcomeAdmin.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './WelcomeAdmin.css'
 
 export default function WelcomeAdmin() {
@@ -8,10 +9,17 @@ export default function WelcomeAdmin() {
             <p><b>Bienvenue !</b></p>
             <p>Que souhaitez vous faire ?</p>
             <div className='button-grid'>
+                <Link to="/admin/formations">
             <Button buttonText='Formations' />
+            </Link> <Link to="/admin/pages">
             <Button buttonText='Pages' />
+            </Link> 
+            <Link to="/admin/stats">
             <Button buttonText='Statistiques' />
+            </Link>
+            <Link to="/admin/settings">
             <Button buttonText='Paramètres du site' />
+            </Link>
             <div className='solid'>
                 </div>
             </div>
