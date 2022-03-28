30fa04407d1112ec37b033459c0e2387cd3d325a
Harry Hauer <geekcat@protonmail.com>
2022-03-28 16:58:48 +0200

    Created formations admin pages

--- client/src/routes/Admin/FormationPage/FormationPage.jsx
+++ client/src/routes/Admin/FormationPage/FormationPage.jsx
@@ -1,9 +1,37 @@
-import react from "react";
+import { React, useEffect, useState } from "react";
 
-function FormationPage() {
-  return <>
-    <table>
-      
-    </table>
-  </>;
+export default function FormationPage() {
+  const [formations, setFormations] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/formations")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setFormations(result.formations);
+        },
+        (error) => {
+          setError(true);
+        }
+      );
+  });
+  return (
+    <>
+      <h1>Formations</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+          </tr>
+        </thead>
+        <tbody>
+          {formations.map((formation) => (
+            <tr value={formation.id}>{formation.name}</tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
