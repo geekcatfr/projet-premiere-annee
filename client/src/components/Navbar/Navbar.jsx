4000ba7ff4a45897e7c057b9ef31b42009552925
Harry Hauer <geekcat@protonmail.com>
2022-03-29 17:57:09 +0200

    Added "se déconnecter" yay

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,36 @@
 import { Outlet, Link } from "react-router-dom";
-import React from "react";
+import { React, useState } from "react";
 
 import "./Navbar.css";
 import logo from "./logo-ndlp.png";
 
 export default function Navbar() {
-  /* const [isConnected, setIsConnected] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
 
   const checkConnection = () => {
     if (localStorage.getItem("token") != null) {
+      setIsConnected(true);
+    } else {
+      setIsConnected(false);
     }
-  }; */
+  };
 
   return (
-    <div>
-      <nav>
-        <div className="navbar">
-          <div className="navbar-left">
-            <img src={logo} alt="logo fcpro" />
-            <Link to="/">FC Pro</Link>
-          </div>
-          <div className="navbar-right">
-            <Link to="/formations">Formations</Link>
-            <Link to="/about">A propos</Link>
-            <Link to="/login">Se connecter</Link>
-          </div>
+    <nav>
+      <div className="navbar">
+        <div className="navbar-left">
+          <img src={logo} alt="logo fcpro" />
+          <Link to="/">FC Pro</Link>
         </div>
-      </nav>
+        <div className="navbar-right">
+          <Link to="/formations">Formations</Link>
+          <Link to="/about">A propos</Link>
+          <Link to="/login">
+            {isConnected ? "Se connecter" : "Se déconnecter"}
+          </Link>
+        </div>
+      </div>
       <Outlet />
-    </div>
+    </nav>
   );
 }
