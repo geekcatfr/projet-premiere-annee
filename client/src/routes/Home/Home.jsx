887c16b1f458dbb8fc0d76047b12175c9c22de12
Harry Hauer <geekcat@protonmail.com>
2022-03-29 17:56:46 +0200

    Moved content from app to home

---
+++ client/src/routes/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home-wrapper">
+      <div className="first-page-content">
+        <p className="first-page-title">Centre de formation FC PRO NDLP</p>
+        <div className="about-buttons">
+          <Link to="/about">
+            <Button name="En savoir plus" />
+          </Link>
+          <Link to="/formations">
+            <Button name="Nos formations" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Button(props) {
+  return <div className="button">{props.name}</div>;
+}
