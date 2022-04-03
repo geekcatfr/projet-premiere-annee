0f662199077de30ca13958c3f22101c04d4faca2
kity <geekcat@protonmail.com>
2022-04-03 14:21:20 +0200

    Decomposed table in multiples components

--- client/src/routes/Admin/FormationPage/FormationPage.jsx
+++ client/src/routes/Admin/FormationPage/FormationPage.jsx
@@ -4,16 +4,27 @@ import "./FormationPage.css";
 
 export default function FormationPage() {
   return (
-    <>
+    <div>
       <h1>Formations</h1>
       <FormationTable />
-    </>
+    </div>
   );
 }
 
 function FormationTable(props) {
   const [formations, setFormations] = useState([]);
   const [error, setError] = useState(null);
+
+  const FormationRows = (props) =>
+    formations.map((formation) => (
+      <FormationRow
+        key={formation.id}
+        formationId={formation.id}
+        title={formation.title}
+      />
+    ));
+
+  const checkAllCaseFormations = () => {};
 
   useEffect(() => {
     fetch("http://localhost:8000/formations")
@@ -35,32 +46,38 @@ function FormationTable(props) {
       <thead>
         <tr>
           <th>
-            <input type="checkbox" />
+            <input type="checkbox" onChange={checkAllCaseFormations} />
           </th>
           <th>Titre</th>
         </tr>
       </thead>
       <tbody>
-        {formations.map((formation) => (
-          <tr className="formation-row" key={formation.id}>
-            <td className="checkbox">
-              <input type="checkbox" />
-            </td>
-
-            <td>
-              <p className="formation-title">{formation.title}</p>
-              <ul className="formation-actions">
-                <li>
-                  <Link to="edit">Editer</Link>
-                </li>
-                <li>
-                  <Link to="delete">Supprimer</Link>
-                </li>
-              </ul>
-            </td>
-          </tr>
-        ))}
+        <FormationRows />
       </tbody>
     </table>
+  );
+}
+
+function FormationRow(props) {
+  const [selected, setIsSelected] = useState(false);
+
+  return (
+    <tr className="formation-row">
+      <td className="checkbox">
+        <input type="checkbox" onChange={() => setIsSelected(!selected)} />
+      </td>
+
+      <td>
+        <p className="formation-title">{props.title}</p>
+        <ul className="formation-actions">
+          <li>
+            <Link to={`edit/${props.formationId}`}>Editer</Link>
+          </li>
+          <li>
+            <Link to={`delete/${props.formationId}`}>Supprimer</Link>
+          </li>
+        </ul>
+      </td>
+    </tr>
   );
 }
