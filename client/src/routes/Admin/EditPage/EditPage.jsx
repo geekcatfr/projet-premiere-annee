f7923a87fe142c4a8a7cdca34011d76fbc3e89e1
kity <geekcat@protonmail.com>
2022-04-04 09:29:58 +0200

    Moved TextArea to a function

--- client/src/routes/Admin/EditPage/EditPage.jsx
+++ client/src/routes/Admin/EditPage/EditPage.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import "./edit.css";
 
-const TextArea = () => (
-  <textarea id="content" placeholder="Entrez du contenu..." />
-);
-
 export default function EditPage() {
   return (
     <div>
       <h1>Editer</h1>
       <div className="title-wrapper">
-        <h2>Titre du contenu</h2>
+        <p>Titre du contenu</p>
         <TextArea />
       </div>
-
+      <div>
+        <p>Contenu</p>
+        <TextArea />
+      </div>
       <button type="submit" onClick={console.log("puss")}>
         Envoyer
       </button>
     </div>
   );
+}
+
+function TextArea(props) {
+  return <textarea id="content" placeholder="Entrez du contenu..." />;
 }
