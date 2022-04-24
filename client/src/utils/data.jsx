8a7b177d0615f950a0750a66a7c6b1ad37d47366
kity <geekcat@protonmail.com>
2022-04-24 22:47:47 +0200

    Created data.jsx file

---
+++ client/src/utils/data.jsx
@@ -0,0 +1,12 @@
+export async function fetchFormations() {
+  const req = await fetch("http://localhost:8000/formations");
+  const formations = await req.json();
+  return formations;
+}
+
+export async function fetchFormation(formationId) {
+  const req = await fetch(`http://localhost:8000/formations/${formationId}`);
+  const formation = await req.json();
+
+  return formation;
+}
