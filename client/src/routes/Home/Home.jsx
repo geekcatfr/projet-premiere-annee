e8f524d08ba783553679746bc9d7526085e1102b
kity <geekcat@protonmail.com>
2022-04-24 22:50:25 +0200

    Removed useless slashes

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -9,10 +9,10 @@ export default function Home() {
       <div className="first-page-content">
         <p className="first-page-title">Centre de formation FC PRO NDLP</p>
         <div className="about-buttons">
-          <Link to="/about">
+          <Link to="about">
             <Button name="En savoir plus" />
           </Link>
-          <Link to="/formations">
+          <Link to="formations">
             <Button name="Nos formations" />
           </Link>
         </div>
