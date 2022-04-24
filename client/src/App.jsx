7a8939b098d251fe6f761841bcd3ddac9da65e54
kity <geekcat@protonmail.com>
2022-04-24 22:46:41 +0200

    Finally figured out how React Router should be used

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,13 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import FormationPage from "./routes/Admin/FormationPage/FormationPage";
-import Formations from "./routes/Formations/Formations";
-import Login from "./routes/Login/Login";
-import About from "./routes/About/About";
-import NotFound from "./routes/NotFound/NotFound";
-
-import WelcomeAdmin from "./routes/Admin/WelcomeAdmin/WelcomeAdmin";
-import Home from "./routes/Home/Home";
-
-import "./App.css";
-import EditPage from "./routes/Admin/EditPage/EditPage";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/formations" element={<Formations />}>
-          <Route path=":id" element={<Formations />} />
-        </Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<WelcomeAdmin />} />
-        <Route path="/admin/formations" element={<FormationPage />} />
-        <Route path="/admin/formations/edit/:id" element={<EditPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Outlet />
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+import FormationPage from "./routes/Admin/FormationPage/FormationPage";
+import Formations from "./routes/Formations/Formations";
+import Login from "./routes/Login/Login";
+import About from "./routes/About/About";
+import NotFound from "./routes/NotFound/NotFound";
 
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import WelcomeAdmin from "./routes/Admin/WelcomeAdmin/WelcomeAdmin";
+import Home from "./routes/Home/Home";
 
+import "./App.css";
 import "normalize.css";
+import EditPage from "./routes/Admin/EditPage/EditPage";
+import App from "./App";
+import Formation from "./routes/Formations/Formation";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="formations" element={<Formations />} />
+          <Route path="formations/:formationId" element={<Formation />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+          <Route path="admin" element={<WelcomeAdmin />} />
+          <Route path="admin/formations" element={<FormationPage />}>
+            <Route path="edit/:id" element={<EditPage />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
