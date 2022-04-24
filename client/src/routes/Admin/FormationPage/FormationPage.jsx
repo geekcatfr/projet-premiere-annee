df16f61853f0d674863357fb89388520d6c22fa3
kity <geekcat@protonmail.com>
2022-04-24 22:49:13 +0200

    Added font awesome icon and buttons

--- client/src/routes/Admin/FormationPage/FormationPage.jsx
+++ client/src/routes/Admin/FormationPage/FormationPage.jsx
@@ -1,30 +1,15 @@
 import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import "./FormationPage.css";
 
-export default function FormationPage() {
-  return (
-    <div>
-      <h1>Formations</h1>
-      <FormationTable />
-    </div>
-  );
-}
+library.add(fas);
 
-function FormationTable() {
+export default function FormationPage() {
   const [formations, setFormations] = useState([]);
   const [error, setError] = useState(null);
-
-  const FormationRows = () =>
-    formations.map((formation) => (
-      <FormationRow
-        key={formation.id}
-        formationId={formation.id}
-        title={formation.title}
-      />
-    ));
-
-  const checkAllCaseFormations = () => {};
 
   useEffect(() => {
     fetch("http://localhost:8000/formations")
@@ -38,31 +23,87 @@ function FormationTable() {
         }
       );
   }, []);
+
   if (error) {
     return <p>Impossible de charger le contenu de la page</p>;
   }
+  if (formations.length === 0) {
+    return (
+      <div>
+        <h1>Formations</h1>
+        <p>
+          Aucune formation existe actuellement. Commencez par en créer une !
+        </p>
+        <AddFormationButton />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Formations</h1>
+      <AddFormationButton />
+      <FormationTable formations={formations} />
+    </div>
+  );
+}
+
+function AddFormationButton() {
+  const addFormationAction = () => {
+    const formationName = prompt("Entrez le nom d'une formation");
+    fetch("http://localhost:8000/formations/add", {
+      method: "POST",
+      mode: "cors",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      body: JSON.stringify({ name: formationName }),
+    });
+  };
+  return (
+    <button
+      onClick={addFormationAction}
+      className="add_formation_button"
+      type="button"
+    >
+      <FontAwesomeIcon icon="fa-solid fa-plus" />
+      <span>Ajouter</span>
+    </button>
+  );
+}
+
+function FormationTable(props) {
+  const { formations } = props;
   return (
     <table>
       <thead>
         <tr>
           <th>
-            <input type="checkbox" onChange={checkAllCaseFormations} />
+            <input type="checkbox" />
           </th>
           <th>Titre</th>
         </tr>
       </thead>
       <tbody>
-        <FormationRows />
+        <FormationRows formations={formations} />
       </tbody>
     </table>
   );
 }
 
+function FormationRows(props) {
+  return props.formations.map((formation) => (
+    <FormationRow
+      key={formation.id}
+      formationId={formation.id}
+      title={formation.title}
+    />
+  ));
+}
+
 function FormationRow(props) {
   const [selected, setIsSelected] = useState(false);
+  const { formationId, title } = props;
 
   const deleteRequest = () => {
-    fetch(`http://localhost:8000/formations/delete/${props.formationId}`);
+    fetch(`http://localhost:8000/formations/delete/${formationId}`);
     window.location.reload();
   };
 
@@ -73,19 +114,15 @@ function FormationRow(props) {
       </td>
 
       <td>
-        <p className="formation-title">{props.title}</p>
+        <p className="formation-title">{title}</p>
         <ul className="formation-actions">
           <li>
-            <Link
-              to={`http://localhost:8000/formations/edit/${props.formationId}`}
-            >
-              Editer
-            </Link>
+            <Link to={`edit/${formationId}`}>Editer</Link>
           </li>
           <li>
-            <a href="#" onClick={deleteRequest}>
+            <button type="button" onClick={deleteRequest}>
               Supprimer
-            </a>
+            </button>
           </li>
         </ul>
       </td>
