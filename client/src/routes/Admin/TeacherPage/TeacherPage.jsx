db3ed537091e377d2a9dd0a49a826c50e503b9ba
kity <geekcat@protonmail.com>
2022-04-26 13:19:20 +0200

    Created TeacherPage route

--- client/src/routes/Admin/TeacherPage/TeacherPage.jsx
+++ client/src/routes/Admin/TeacherPage/TeacherPage.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 
 export default function TeacherPage() {
-    return(
-        <>
-            <h1>Liste d'enseignants</h1>
-            
-        </>
-    )
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/teachers")
+      .then((res) => res.json())
+      .then((res) => setTeachers);
+  });
+  return (
+    <>
+      <h1>Liste d'enseignants</h1>
+      <TeachersTable teachers={teachers} />
+    </>
+  );
+}
+
+function TeachersTable({ teachers }) {
+  return (
+    <table>
+      <thead>
+        <th>Sélectionner</th>
+        <th>Nom</th>
+        <th>Prénom</th>
+      </thead>
+      <tbody>
+        <tr>{teachers.map()}</tr>
+      </tbody>
+    </table>
+  );
 }
