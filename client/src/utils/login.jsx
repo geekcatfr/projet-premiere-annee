4783061b455c84ee27c806a24e90487c609dac6f
kity <geekcat@protonmail.com>
2022-05-02 22:12:34 +0200

    Added user functions

--- client/src/utils/login.jsx
+++ client/src/utils/login.jsx
@@ -4,3 +4,23 @@ export function checkLoginTokenStorage() {
   }
   return true;
 }
+
+export const addNewUser = (username, password, isAdmin) => {
+  const headers = new Headers({ "Content-Type": "application/json" });
+  fetch("http://localhost:8000/users/add", {
+    method: "POST",
+    cors: "cors",
+    headers,
+    body: JSON.stringify({ username, password, isAdmin }),
+  });
+};
+
+export const deleteUser = (userId) => {
+  const headers = new Headers({ "Content-Type": "application/json" });
+  fetch("http://localhost:8000/users/delete", {
+    method: "POST",
+    cors: "cors",
+    headers,
+    body: JSON.stringify({ userId }),
+  });
+};
