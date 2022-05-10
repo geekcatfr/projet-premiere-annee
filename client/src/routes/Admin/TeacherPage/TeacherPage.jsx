1e87e21d4147898b880df689358c06748458158e
kity <geekcat@protonmail.com>
2022-05-10 15:01:44 +0200

    Added delete button

--- client/src/routes/Admin/TeacherPage/TeacherPage.jsx
+++ client/src/routes/Admin/TeacherPage/TeacherPage.jsx
@@ -1,31 +1,45 @@
 import PropTypes from "prop-types";
 import { React, useEffect, useState } from "react";
+import { AddTeacherButton } from "../../../components/Button/Button";
 
 export default function TeacherPage() {
   const [teachers, setTeachers] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:8000/teachers")
       .then((res) => res.json())
-      .then((res) => setTeachers(res));
+      .then((res) => {
+        setTeachers(res);
+        setIsLoaded(true);
+      });
   }, []);
+  if (!isLoaded) {
+    return <p>Chargement des données...</p>;
+  }
   return (
     <div>
       <h1>Liste d&apos;enseignants</h1>
-      <select>
-        <option>Sélectionner...</option>
-        <option>Supprimer</option>
-      </select>
-      <input type="button" />
+      <OptionSelector />
+      <AddTeacherButton />
       <TeachersTable teachers={teachers} />
     </div>
   );
 }
 
+function OptionSelector() {
+  return (
+    <div className="optionSelector">
+      <select>
+        <option>Sélectionner...</option>
+        <option>Supprimer</option>
+      </select>
+      <input type="button" value="Executer" />
+    </div>
+  );
+}
+
 function TeachersTable({ teachers }) {
-  const deleteAction = (teacherId) => {
-    fetch(`http://localhost:8000/teachers/delete?teacherId=${teacherId}`);
-  };
   const TableRows = teachers.map((teacher) => (
     <tr key={teacher.id}>
       <td>
@@ -35,11 +49,7 @@ function TeachersTable({ teachers }) {
       <td>{teacher.lastName}</td>
       <td className="actionBox">
         <input type="button" value="Editer" />
-        <input
-          onClick={deleteAction(teacher.id)}
-          type="button"
-          value="Supprimer"
-        />
+        <DeleteButton teacherId={teacher.id} />
       </td>
     </tr>
   ));
@@ -57,4 +67,15 @@ function TeachersTable({ teachers }) {
 }
 TeachersTable.propTypes = {
   teachers: PropTypes.array.isRequired,
+};
+
+function DeleteButton({ teacherId }) {
+  const deleteAction = () => {
+    fetch(`http://localhost:8000/teachers/delete/${teacherId}`);
+  };
+  return <input onClick={deleteAction} type="button" value="Supprimer" />;
+}
+
+DeleteButton.propTypes = {
+  teacherId: PropTypes.number.isRequired,
 };
